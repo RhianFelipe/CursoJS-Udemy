f7026a8846b9c0f72a7e04b0c65dfcf531d9eede
RhianFelipe <rhian.felipe@hotmail.com>
2021-01-27 13:21:39 -0300

    classe

--- FUNDAMENTOS/funcao1.js
+++ FUNDAMENTOS/funcao1.js
@@ -2,12 +2,12 @@
 function imprimirSoma (a , b , c) {
 console.log(a + b + c)
 }
-imprimirSoma(2,5)
+imprimirSoma(2,5,6)
 imprimirSoma(2) // 2 + undefined = NaN
 imprimirSoma(2,3,4,5,6,7,8) //ele só vai pegar o A e B e o resto vai ignorar
 imprimirSoma()
 //Função com retorno
-function soma(a , b = 0) {
+function soma(a , b) {
 return a + b
 
 }
