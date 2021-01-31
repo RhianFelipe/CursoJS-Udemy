ac7933166736622f7327c57813783835a2b6d4e2
RhianFelipe <rhian.felipe@hotmail.com>
2021-01-31 16:06:53 -0300

    Objeto

--- OBJETO/classe.js
+++ OBJETO/classe.js
@@ -1,1 +1,33 @@
-//a fazer ainda
+class Lancamento{
+    constructor(nome = 'Genérico', valor = 0){
+        this.nome = nome
+        this.valor = valor
+    }
+
+}
+
+class CicloFinanceiro {
+    constructor(mes,ano){
+        this.mes = mes
+        this.ano = ano
+        this.lancamentos  = []
+    }
+    addLancamentos(...lancamentos){
+        lancamentos.forEach(l => this.lancamentos.push(l)) //pegar todos os elementos na array,e juntando todos
+    }
+    sumario(){
+        let valorConsolidado = 0
+        this.lancamentos.forEach(l =>{
+            valorConsolidado += l.valor
+        })
+      return valorConsolidado
+    }
+}
+
+ 
+const salario = new Lancamento('Salario', 45000)
+const contaLuz = new Lancamento('Luz', -220)
+
+const contas = new CicloFinanceiro(6,2018)
+contas.addLancamentos(salario,contaLuz)
+console.log(contas.sumario())
