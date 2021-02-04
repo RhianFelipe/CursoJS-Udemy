1b11ccff5269342ee67ae89a5dfabdc57cd4e4e7
RhianFelipe <rhian.felipe@hotmail.com>
2021-02-04 15:32:42 -0300

    Array

---
+++ ARRAY/arrayMetodos.js
@@ -0,0 +1,12 @@
+const pilotos = ['Vettel','Alonso', 'Raikkonen','Massa']
+pilotos.pop() //massa quebrou o carro! , pop remove o ultimo elemento da Array
+console.log(pilotos)
+
+pilotos.push('Verstappen') //push adiciona um novo elemento
+console.log(pilotos)
+
+pilotos.shift() // remove o primeiro!
+console.log(pilotos)
+
+pilotos.unshift('Hamilton') //adciona o primeiro elemento na array
+console.log(pilotos) 
